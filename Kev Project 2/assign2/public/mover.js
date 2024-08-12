d29e0dfb99cd03bb2518aac9df3983fe9bc95979
danantony06 <danantony06@gmail.com>
2024-08-12 17:47:30 -0500

    KanbanBoard

---
+++ Kev Project 2/assign2/public/mover.js
@@ -0,0 +1,24 @@
+/* Text to add to the move here button */
+const MOVE_HERE_TEXT = "— Move here —";
+
+export default class Mover {
+  constructor() {
+    //TODO
+  }
+
+  startMoving(card) {
+    this.card = card;
+
+
+    this.card.remove();
+    
+    
+
+  }
+
+  stopMoving() {
+    //TODO
+  }
+
+  //TODO
+}
